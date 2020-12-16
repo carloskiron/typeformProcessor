1d1ad7df8aee92334638561d438f07e0225e774c
Carlos <carlos@stellaremploy.com>
2020-12-15 19:48:26 -0500

    removing fields not required

--- src/main/java/com/processor/domain/QuestionnaryDataPoint.java
+++ src/main/java/com/processor/domain/QuestionnaryDataPoint.java
@@ -2,24 +2,9 @@ package com.processor.domain;
 
 public class QuestionnaryDataPoint extends BaseObject {
 
-    private String entryId;
-    private String formId; //the id of the form in the form system used by stellar
-    private String name;
-    private String applicantId;
-    private int companyLocationCode;
-    private int companyCode;
-    private int jobPositionCode;
-
+    private String formId;
     public QuestionnaryDataPoint() {
         super();
-    }
-
-    public String getEntryId() {
-        return entryId;
-    }
-
-    public void setEntryId(String entryId) {
-        this.entryId = entryId;
     }
 
     public String getFormId() {
@@ -29,45 +14,4 @@ public class QuestionnaryDataPoint extends BaseObject {
     public void setFormId(String formId) {
         this.formId = formId;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCompanyLocationCode() {
-        return companyLocationCode;
-    }
-
-    public void setCompanyLocationCode(int companyLocationCode) {
-        this.companyLocationCode = companyLocationCode;
-    }
-
-    public int getCompanyCode() {
-        return companyCode;
-    }
-
-    public void setCompanyCode(int companyCode) {
-        this.companyCode = companyCode;
-    }
-
-    public int getJobPositionCode() {
-        return jobPositionCode;
-    }
-
-    public void setJobPositionCode(int jobPositionCode) {
-        this.jobPositionCode = jobPositionCode;
-    }
-
-    public String getApplicantId() {
-        return applicantId;
-    }
-
-    public void setApplicantId(String applicantId) {
-        this.applicantId = applicantId;
-    }
-
 }
